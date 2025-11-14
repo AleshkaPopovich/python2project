17412e0766aa79e0364ba28c1a8dc3e65da1a391
AleshkaPopovich <aleksey.ganeev123@gmail.com>
2025-11-14 19:13:32 +0100

    Weather tg app description

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # python2project
 TELEGRAM BOT
 
-AI HARBOUR SPACE HELPER CHATBOT
+HARBOUR SPACE WEATHER BOT
+
+DESCRIPTION
+Automatic telegram bot which will be sending weather news everyday at a fixed time
+Will be accepting input from the user about the quality of the weather in a given location at a given time
+Will be providing insights about the extent of safety of the outdoor activites 
--- python_project.py
+++ python_project.py
@@ -1,5 +1,18 @@
-numbers = (0, 1, 3, 14, 2, 7, 9, 8, 10)
-print(numbers)
-input1 = int(input())``
 
-print("hello world")
+def get_input():
+    ...
+    return counted_dict
+
+def encrypt():
+    reutrn enc_resilt
+    
+def output():
+    pass
+
+def main():
+    text = file("plain.txt")
+    counted_dict = get_input(text)
+    enc_result = encrypt(counted_dict)
+    ouput(encresult)
+
+main()
